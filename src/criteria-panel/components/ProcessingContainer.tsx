ae93b7a2422cab3f451ba894c575ea29c18b092a
Joachim Dalen <me@d3v.no>
2022-04-06 21:24:04 +0200

    fix: Approval flow

--- src/criteria-panel/components/ProcessingContainer.tsx
+++ src/criteria-panel/components/ProcessingContainer.tsx
@@ -14,7 +14,7 @@ const ProcessingContainer = ({
   criteriaId,
   processCriteria
 }: ProcessingContainerProps): JSX.Element => {
-  const [selected, setSelected] = useState<string>();
+  const [selected, setSelected] = useState<string>('dummy');
   return (
     <div className="flex-grow padding-horizontal-16 padding-bottom-8 padding-top-4 font-size-s dark-background">
       <h3>This criteria needs your attention</h3>
@@ -33,11 +33,12 @@ const ProcessingContainer = ({
           <RadioButton id="reject" text="Reject" key="reject" />
         </RadioButtonGroup>
         <Button
+          disabled={selected === 'dummy'}
           text="Save"
           primary
           iconProps={{ iconName: 'Save' }}
           onClick={async () => {
-            await processCriteria(criteriaId, selected === 'Approve');
+            await processCriteria(criteriaId, selected === 'approve');
           }}
         />
       </div>
